b38c6d67efd6f05826d25cb88afbed751b53fdce
pasindusiriwardana <pasindulasithjk@gmail.com>
2019-05-08 19:19:57 +0530

    Admin dashboard added

---
+++ Frontend/the-london-cut/src/app/components/admin/admin.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-admin',
+  templateUrl: './admin.component.html',
+  styleUrls: ['../../../assets/css/material-dashboard.min.css']
+})
+export class AdminComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
